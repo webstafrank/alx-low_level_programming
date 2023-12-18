8d4f2f9a6d01b70c5a17ebc62873ff8372dd363f
webstafrank <franklyndenish@gmail.com>
2023-12-18 01:04:33 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	char letter;
+
+	/*creating variable of type char and looping thru ther alphabets*/
+
+		for (letter='z'; letter>='a'; letter++)
+					
+			putchar(letter);
+		
+		putchar('\n');
+
+	return (0);
+
+}
