bcaf23f294f3499240083b61f7f1f4bdd644331b
webstafrank <franklyndenish@gmail.com>
2023-12-17 23:50:49 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	/*initializing an integer and setting the range of loop*/
+
+	for (n=0; n<10; n++)
+	{
+		printf("%d", n);
+		printf("\n");
+	}
+	return 0;
+
+}
+	
