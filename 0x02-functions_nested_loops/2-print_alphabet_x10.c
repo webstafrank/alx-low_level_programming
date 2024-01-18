6170c20b12981098e580b7cff9ba879f0c4b2f69
webstafrank <franklyndenish@gmail.com>
2024-01-18 06:18:12 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/**File:2-print_alphabet_x10.c
+ * Auth:Franklyn Denish*/
+
+#include"main.h"
+
+void print_alphabet_x10(void)
+{
+	int range=0;
+	char letter;
+
+	/*a function that prints 10 times the alphabet, in lowercase, followed by a new line*/
+		
+		while(range++ <=9)
+		{
+			for(letter ='a';letter <= 'z'; letter++)
+				_putchar(letter);
+			_putchar('\n');
+		}
+}
