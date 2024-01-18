62777aeb5f0582ded06810d2800d803471e6140a
webstafrank <franklyndenish@gmail.com>
2024-01-18 05:30:44 -0800

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**program prints _putchar followed by a new line
+ *
+ * file: 0-putchar.c
+ * Auth: Franklyn Denish*/
+
+#include "main.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+
+}
