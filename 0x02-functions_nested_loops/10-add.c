fc5be412d4dfb4a363281d1442f358b97a9e20d0
webstafrank <franklyndenish@gmail.com>
2024-01-24 17:23:44 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**a function that adds two integers and returns the result.
+ * 
+ * Prototype: int add(int, int);
+ *
+ * returns an inter
+ */
+int add(int digit1, int digit2)
+{
+	return (digit1 + digit2);
+}
+
