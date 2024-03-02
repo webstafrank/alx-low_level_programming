84f25230f0730bbd0f82fb530e4b35c64ffa0b8e
webstafrank <franklyndenish@gmail.com>
2024-03-02 06:07:54 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
- * * Descrtiption - main prints a string to the stdout using puts
- * * return : Always 0; 
+ *
+ *  Descrtiption - main prints a string to the stdout using puts
+ * 
+ *  return : Always 0; 
  */
 int main(void)
 {	
-	puts("programming is like building a multilingial puzzle\n");
+	puts("programming is like building a multilingial puzzle");
 	return (0);
 }
