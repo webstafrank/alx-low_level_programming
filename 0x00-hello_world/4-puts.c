9353509ac61a5d088864455d8aba840083eb4d42
webstafrank <franklyndenish@gmail.com>
2024-03-02 05:45:39 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints an to the stdout using puts
+ *
+ * return: 0
+ */
 int main(void)
 {	
-	/**
-	 * printing with puts
-	 * returns 0
-	 */
 	puts("programming is like building a multilingial puzzle\n");
-	
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
-int main() {
-       int x;
-	int y;       
+/**
+ * printing the sizes of data types
+ * arg - sizeof
+ * returns 0
+ */
+int main(void)
+{
+	printf("Size of a char:%lu\n", sizeof(char));
+	printf("Size of a int:%lu\n", sizeof(int));
+	printf("Size of a long int:%lu\n", sizeof(long int));
+	printf("Size of a long long int:%lu\n", sizeof(long long int));
+	printf("Size of a float:%lu\n", sizeof(float));
+	
+	return (0);
+		
 
