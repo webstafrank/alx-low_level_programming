c368d72bf74c9a9aab483eceaf938f745811673a
webstafrank <franklyndenish@gmail.com>
2024-03-06 13:45:41 -0800

    alphabet soup

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point.
+ * Description : main prints alphabets in lowercase followed by a new line
+ *
+ * Return: 0 on success.
+ */
+int main(void)
 {
 	/* initializing a as first variable in the expansion [a-z]*/
 
-	char alphabet='a';
+	char alphabet = 'a';
 
 	/*looping to check whether to print for the next alphabet*/
-
-	while (alphabet <= 'z')
-	
+	while (alphabet <= 'z')	
 	{
-	/*print the variables and increase by one alphabet till z*/	
-
+	/*print the variables and increase by one alphabet till z*/
 		putchar(alphabet);
 		alphabet++;
-
 	}
-
 	putchar('\n');
-	
-	return 0;
-
+	return (0);
 }
-
-
