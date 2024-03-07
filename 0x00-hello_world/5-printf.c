3416c02177c44e9c1ae61bc1b4b2f05bbb6f1b42
webstafrank <franklyndenish@gmail.com>
2024-03-07 14:31:26 -0800

    "Hello, printf"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ * Description - using printf to output a string
  *
- * Description - using printf to print
- *
- * return - main returns 1 on sucess and 0 on failure
+ * Return: main returns 1 on sucess and 0 on failure
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n:");
