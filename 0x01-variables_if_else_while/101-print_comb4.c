e19e786a1321dbf7d92e8912cdb8b0bddbac7844
webstafrank <franklyndenish@gmail.com>
2024-03-07 13:51:04 -0800

    "The success combination in business is: Do what you do better... and: do more of what you do..."

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,33 @@
-/**File: 101-print_comb4.c
- * Auth: Franklyn Denish*/
-
 #include <stdio.h>
+
+/** main - Entry point
+ * Description: the function prints all possible different combinations of three digits.
+ * Numbers must be separated by ,, followed by a space
+ * The three digits must be different
+ * 
+ * Return: always 0 (successs)
+ */
 
 int main(void)
 {
-        int num1, num2, num3;;
-   
-        /**this program prints all possible combination of 3 digits separated by commas, followed by space*/
+        int num1, num2, num3;
 
-        for ( num1=0; num1<8; num1++)
+        for (num1 = 0; num1 < 8; num1++)
         {
-                for(num2=num1+1; num2<9; num2++)
-
+                for (num2 = num1 + 1; num2 < 9; num2++)
                 {
-                	for(num3=num2+2; num3<8; num3++)
-			
-			{	putchar((num1%10)+ '0');
+                	for (num3 = num2 + 2; num3 < 8; num3++)
+			{
+				putchar((num1 % 10) + '0');
 
-                       		putchar((num2%10)+ '0');
+                       		putchar((num2 % 10) + '0');
 				
-				putchar((num3%10)+ '0');
+				putchar((num3 % 10)+ '0');
 
-                       		 if (num1==7 && num2==8 && num3==9)
-
+                       		 if (num1 == 7 && num2 == 8 && num3 == 9)
                                 	continue;
-				 
 
-                       		 putchar(',');
+				 putchar(',');
 
                        		 putchar(' ');
 
