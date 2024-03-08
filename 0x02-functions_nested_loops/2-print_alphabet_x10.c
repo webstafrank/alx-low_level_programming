e053e9d368d0e57b604ed5acb8ec5ac4aff16424
webstafrank <franklyndenish@gmail.com>
2024-03-08 01:59:09 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,13 @@
 #include"main.h"
+
 /**
- * pvoid print_alphabet_x10(void):prints 10 times the alphabet  in lowercase, followed by a new line
- *
  * Description: a function that prints 10 times the alphabet  in lowercase
  * prototype: void print_alphabet_x10(void)
- * Return: void
+ *
+ * Return: 0 on success
+ *
  */
+
 void print_alphabet_x10(void)
 {
 	int range = 0;
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,12 @@
 #include "main.h"
-/**File: 5-sign.c5-sign.c
- *
- * Auth: Franklyn Denish
- *
- * Descr: a function that prints the sign of a number
- *
- * Return: 1 for +ve number, 0 for 0 and -1 for others*/
-
+/**
+ * print_sign: a function that prints the sign of a number
+ * * prototype: int print_sign(int n)
+ * Return: 1 for +ve number, 0 for 0 and -1 for others
+ */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n > 0) 
 	{
 		_putchar(43);
 		return(1);
