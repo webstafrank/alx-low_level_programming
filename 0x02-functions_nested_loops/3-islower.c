4a4e33f5da2078bf749e4dfef5abc368d91e6672
webstafrank <franklyndenish@gmail.com>
2024-03-08 14:24:57 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
-/**File: 3-islower.c
- * Auth: Franklyn Denish
- *
- *Desc: a function that checks for lowercase character*/
-
 #include "main.h"
+
+/**
+ * Description: a function that checks for lowercase character.
+ * parameter: int _islower(int c)
+ *
+ * Return: None (void)
+ */
 
 int _islower(int c)
 {
-	if (c >= 97 &&  c<= 122)
+	if (c >= 97 &&  c <= 122)
 	{
 		return (1);
 	}
-
 	return (0);
 }
