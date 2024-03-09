b15f2b4bfc780287e22ed20eedd336a119201e35
webstafrank <franklyndenish@gmail.com>
2024-03-09 01:27:59 -0800

    Write a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
- *Description: a function that prints the last digit of a number
- *@arg: n
- * return: last1
+ *Description:
+ a function that prints the last digit of a number parameter: n
+ *
+ * Return: last1
  */
+
 int print_last_digit(int n)
 {
 	int last1 = n % 10;
+
 	if (n < 0)
 	last1 *= -1;
-    	_putchar(last1 + '0');
+	
+	_putchar(last1 + '0');
 	return (last1);
 }
 
