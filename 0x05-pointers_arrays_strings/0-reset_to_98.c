2b3fb349044a6ae2cf3dc02670197901b6983df9
webstafrank <franklyndenish@gmail.com>
2024-04-15 07:39:23 -0700

    Street Bat

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,13 @@
 
 /*
  * 0-reset_to _98.c - Updates the value pointed to by a pointer to 98.
- * 
  * @n: pointer to an integer.
+ *
+ * Description: This function takes a pointer to an int as parameter
+ * updates it value to 98
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;	
 }
-
